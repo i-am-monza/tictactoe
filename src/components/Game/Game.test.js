3e9be95dbcb2ec9127834cca3ecdabf633197acf
Monza <monwabisidingane@gmail.com>
2020-05-14 11:52:36 +0200

    Updating tests. Starting with Game component, all others are empty.

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import test_renderer  from 'react-test-renderer';
-import { Game } from './Game.jsx';
+import renderer from 'react-test-renderer';
+import { Game } from 'Game.jsx';
 
-describe('test cases for the Game component', () => {
-  test('test if this component is rendered', () => {
-    ReactDOM.render(<Game></Game>, document.createElement("div"));
-  });
-
-  test('test snapshot version', () => {
-    expect(test_renderer.create(<Game></Game>)).toMatchSnapshot();
-  });
+describe("test suite for Game.jsx component", () => {
+    test("Game component snapshot is the same with in repository", () => {
+        const tree = renderer.create(<Game></Game>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,4 @@
-// import extended matchers for testing library
-import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
