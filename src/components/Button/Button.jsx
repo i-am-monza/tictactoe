9f09d89c3da215c88a40b4b486d43160e7d380c1
Monza <monwabisidingane@gmail.com>
2020-05-18 13:36:23 +0200

    fix: Add code formatting tool and rules

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 // functional component Button accepting properties
-export const Button = props => {
-    return (
-        <button 
-            id={`${props.index}`}
-            type="button" 
-            title={ `Button ${props.index + 1}` }
-            onClick={event => props.handleClick(event)}>
-                { props.getHistory(props.index) }
-        </button>);
-}
+export const Button = (props) => {
+  return (
+    <button
+      id={`${props.index}`}
+      type="button"
+      title={`Button ${props.index + 1}`}
+      onClick={(event) => props.handleClick(event)}
+    >
+      {props.getHistory(props.index)}
+    </button>
+  );
+};
